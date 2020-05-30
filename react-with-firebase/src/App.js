febc938173d624a4dc23dcbb37bca531bf60b527
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-05-30 20:10:47 +0530

    do implementation inside App.js file

--- react-with-firebase/src/App.js
+++ react-with-firebase/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import fire from './config/fire';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {}
+    }
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user })
+      } else {
+        this.setState({ user: null })
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.user ? <Home/> : <Login/>}
+      </div>
+    )
+  }
 }
 
 export default App;
