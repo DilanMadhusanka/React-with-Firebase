67b2e0833e1ef332c6215ff99f9e289a8a3a0e01
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-05-31 11:14:02 +0530

    create logout method inside Home component for signout

--- react-with-firebase/src/components/Home.js
+++ react-with-firebase/src/components/Home.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import fire from '../config/fire';
 
 class Home extends React.Component {
+
+    logout() {
+        fire.auth().signOut();
+    }
+
     render() {
         return (
             <div>
                 <h1>You are logged in !!!</h1>
-                <button>Logout</button>
+                <button onClick={this.logout}>Logout</button>
             </div>
         )
     }
