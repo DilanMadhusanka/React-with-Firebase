924359505907d36067830b84ab5a7c903acd8da2
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-06-01 22:18:02 +0530

    create gui for contact form using jsx

--- react-with-firebase/src/components/ContactForm.js
+++ react-with-firebase/src/components/ContactForm.js
@@ -1,7 +1,42 @@
-import React from 'react';
-const ContactForm = () => {
+import React, { useState } from 'react';
+
+const ContactForm = (props) => {
+
     return (
-        <div>display form</div>
+        <form autoComplete="off">
+            <div className="form-group input-group">
+                <div className="input-group-prepend">
+                    <div className="input-group-text">
+                        <i className="fas fa-user"></i>
+                    </div>
+                </div>
+                <input className="form-control" placeholder="Full Name" name="fullName" />
+            </div>
+            <div className="form-row">
+                <div className="form-group input-group col-md-6">
+                    <div className="input-group-prepend">
+                        <div className="input-group-text">
+                            <i className="fas fa-mobile-alt"></i>
+                        </div>
+                    </div>
+                    <input className="form-control" placeholder="Mobile" name="mobile" />
+                </div>
+                <div className="form-group input-group col-md-6">
+                    <div className="input-group-prepend">
+                        <div className="input-group-text">
+                            <i className="fas fa-envelope"></i>
+                        </div>
+                    </div>
+                    <input className="form-control" placeholder="Email" name="email" />
+                </div>
+            </div>
+            <div className="form-group">
+                <textarea className="form-control" placeholder="Address" name="address" />
+            </div>
+            <div className="form-group">
+                <input type="submit" value="Save" className="btn btn-primary btn-block" />
+            </div>
+        </form>
     );
 }
 
--- react-with-firebase/src/components/Contacts.js
+++ react-with-firebase/src/components/Contacts.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ContactForm from "./ContactForm"
+import firebase from '../config/fire'
+
+const firebaseDb = firebase.database().ref();
 
 const Contacts = () => {
     return (
         <>
-            <div class="jumbotron jumbotron-fluid">
-                <div class="container">
-                    <h1 class="display-4 text-center">Contact Register</h1>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4 text-center">Contact Register</h1>
                 </div>
             </div>
             <div className="row">
                 <div className="col-md-5">
-                    <ContactForm />
+                    <ContactForm addOrEdit={addOrEdit} />
                 </div>
                 <div className="col-md-7">
                     <div>list of contacts</div>
