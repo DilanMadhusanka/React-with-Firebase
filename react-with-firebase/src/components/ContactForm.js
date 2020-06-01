b9840925e865492ec3ddbcf4589d509331dbf3fa
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-06-01 22:20:30 +0530

    create state objects and set input value using state object

--- react-with-firebase/src/components/ContactForm.js
+++ react-with-firebase/src/components/ContactForm.js
@@ -2,6 +2,15 @@ import React, { useState } from 'react';
 
 const ContactForm = (props) => {
 
+    const initialFieldValues = {
+        fullName: '',
+        mobile: '',
+        email: '',
+        address: ''
+    }
+
+    var [values, setValues] = useState(initialFieldValues)
+
     return (
         <form autoComplete="off">
             <div className="form-group input-group">
@@ -10,7 +19,7 @@ const ContactForm = (props) => {
                         <i className="fas fa-user"></i>
                     </div>
                 </div>
-                <input className="form-control" placeholder="Full Name" name="fullName" />
+                <input className="form-control" placeholder="Full Name" name="fullName" value={values.fullName} />
             </div>
             <div className="form-row">
                 <div className="form-group input-group col-md-6">
@@ -19,7 +28,7 @@ const ContactForm = (props) => {
                             <i className="fas fa-mobile-alt"></i>
                         </div>
                     </div>
-                    <input className="form-control" placeholder="Mobile" name="mobile" />
+                    <input className="form-control" placeholder="Mobile" name="mobile" value={values.mobile} />
                 </div>
                 <div className="form-group input-group col-md-6">
                     <div className="input-group-prepend">
@@ -27,11 +36,11 @@ const ContactForm = (props) => {
                             <i className="fas fa-envelope"></i>
                         </div>
                     </div>
-                    <input className="form-control" placeholder="Email" name="email" />
+                    <input className="form-control" placeholder="Email" name="email" value={values.email} />
                 </div>
             </div>
             <div className="form-group">
-                <textarea className="form-control" placeholder="Address" name="address" />
+                <textarea className="form-control" placeholder="Address" name="address" value={values.address} />
             </div>
             <div className="form-group">
                 <input type="submit" value="Save" className="btn btn-primary btn-block" />
